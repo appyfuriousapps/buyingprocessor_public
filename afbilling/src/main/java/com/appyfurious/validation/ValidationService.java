75e719f134055972663ece1550cc8931532fb2f8
Oleg Davidovich <o.davidovich@appyfurious.com>
2018-06-04 11:23:13 +0300

    add validation

---
+++ afbilling/src/main/java/com/appyfurious/validation/ValidationService.java
@@ -0,0 +1,26 @@
+package com.appyfurious.validation;
+
+import com.appyfurious.validation.body.ValidationBody;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * ValidationService.java
+ * getfitandroid
+ * <p>
+ * Created by o.gay on 25.05.2018.
+ * <p>
+ * Copyright © 2018 Appyfurious. All rights reserved.
+ */
+
+public interface ValidationService {
+
+    @POST("android/verify")
+    Call<ResponseBody> validate(@Query("api_key") String apiKey,
+                                @Body ValidationBody body);
+
+}
