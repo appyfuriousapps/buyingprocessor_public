ff92c3fdf350acb408ca7e1ce154a75363896cc1
bobrovappyfurious <n.bobrov@appyfurious.com>
2018-06-05 14:30:59 +0300

    add DeviceDAta toString

--- afbilling/src/main/java/com/appyfurious/validation/body/DeviceData.java
+++ afbilling/src/main/java/com/appyfurious/validation/body/DeviceData.java
@@ -13,8 +13,10 @@ import com.google.gson.annotations.SerializedName;
 
 public class DeviceData {
 
-    @SerializedName("appsflyerId") private String appsflyerId;
-    @SerializedName("idfa") private String idfa;
+    @SerializedName("appsflyerId")
+    private String appsflyerId;
+    @SerializedName("idfa")
+    private String idfa;
 
     public DeviceData(String appsflyerId, String idfa) {
         this.appsflyerId = appsflyerId;
@@ -37,4 +39,8 @@ public class DeviceData {
         this.idfa = idfa;
     }
 
+    @Override
+    public String toString() {
+        return "appsflyerId:" + appsflyerId + " idfa:" + idfa;
+    }
 }
