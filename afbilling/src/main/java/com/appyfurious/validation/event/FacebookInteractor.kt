0c001ae923e71b8bfcbe8844af716e329dbdc835
bobrovappyfurious <n.bobrov@appyfurious.com>
2018-06-20 14:34:23 +0300

    add facebook logs

--- afbilling/src/main/java/com/appyfurious/validation/event/FacebookInteractor.kt
+++ afbilling/src/main/java/com/appyfurious/validation/event/FacebookInteractor.kt
@@ -1,7 +1,7 @@
 package com.appyfurious.validation.event
 
 import android.content.Context
-
+import android.os.Bundle
 import com.facebook.appevents.AppEventsConstants
 import com.facebook.appevents.AppEventsLogger
 
@@ -18,11 +18,57 @@ import com.facebook.appevents.AppEventsLogger
 
 object FacebookInteractor {
 
-    fun logAddedToCartEvent(context: Context?) {
+    const val PREMIUM_SCREEN_SHOWN = "Premium_Screen_Shown"
+    const val PREMIUM_OPTION_SELECTED = "Premium_Option_Selected"
+
+    private const val SEGMENT_ID = "Segment ID"
+    private const val SCREEN_ID = "Screen ID"
+    private const val SOURCE = "Source"
+    private const val PRODUCT_ID = "Product ID"
+    private const val PREMIUM_OPTION_PURCHASED = "Premium_Option_Purchased"
+
+    private fun logger(context: Context?, body: (AppEventsLogger) -> Unit) {
         context?.let {
-            val logger = AppEventsLogger.newLogger(it)
-            logger.logEvent(AppEventsConstants.EVENT_NAME_ADDED_TO_CART)
+            body(AppEventsLogger.newLogger(it))
         }
     }
 
+    fun logAddedToCartEvent(context: Context?) {
+        logger(context) {
+            it.logEvent(AppEventsConstants.EVENT_NAME_ADDED_TO_CART)
+
+        }
+    }
+
+    fun logPremiumScreenShownEvent(context: Context?, screenName: String, callScreenName: String) {
+        logger(context) {
+            it.logEvent(PREMIUM_SCREEN_SHOWN, Bundle().apply {
+                putString(SEGMENT_ID, "default")
+                putString(SCREEN_ID, screenName)
+                putString(SOURCE, callScreenName)
+            })
+        }
+    }
+
+    fun logPremiumOptionSelectedEvent(context: Context?, screenName: String, callScreenName: String, productId: String) {
+        logger(context) {
+            it.logEvent(PREMIUM_OPTION_SELECTED, Bundle().apply {
+                putString(SEGMENT_ID, "default")
+                putString(SCREEN_ID, screenName)
+                putString(SOURCE, callScreenName)
+                putString(PRODUCT_ID, productId)
+            })
+        }
+    }
+
+    fun logPremiumOptionPurchasedEvent(context: Context?, screenName: String, callScreenName: String, productId: String) {
+        logger(context) {
+            it.logEvent(PREMIUM_OPTION_SELECTED, Bundle().apply {
+                putString(SEGMENT_ID, "default")
+                putString(SCREEN_ID, screenName)
+                putString(SOURCE, callScreenName)
+                putString(PRODUCT_ID, productId)
+            })
+        }
+    }
 }
