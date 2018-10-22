5127463dbb9ad72765f69321873a4439ff83ac17
Oleg Davidovich <o.davidovich@appyfurious.com>
2018-10-22 11:54:33 +0300

    change position of default rewarded progress

--- afbilling/src/main/java/com/appyfurious/ad/RewardedLoadingProgressDefault.java
+++ afbilling/src/main/java/com/appyfurious/ad/RewardedLoadingProgressDefault.java
@@ -1,9 +1,14 @@
 package com.appyfurious.ad;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
+
+import com.appyfurious.afbilling.R;
 
 /**
  * RewardedLoadingProgressDefault.java
@@ -22,7 +27,7 @@ public class RewardedLoadingProgressDefault implements RewardedLoadingProgressLi
 
     public RewardedLoadingProgressDefault(Context context) {
         mContext = context;
-        mProgressBar = new ProgressBar(mContext, null, android.R.attr.progressBarStyleHorizontal);
+        mProgressBar = new ProgressBar(mContext, null, R.style.RewardedProgressStyle);
         mProgressBar.setIndeterminate(true);
     }
 
@@ -31,6 +36,12 @@ public class RewardedLoadingProgressDefault implements RewardedLoadingProgressLi
         if (mContext instanceof AppCompatActivity) {
             AppCompatActivity rewardedActivity = (AppCompatActivity) mContext;
             mRootView = rewardedActivity.findViewById(android.R.id.content);
+
+            FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) mRootView.getLayoutParams();
+            params.width = FrameLayout.LayoutParams.MATCH_PARENT;
+            params.height = FrameLayout.LayoutParams.WRAP_CONTENT;
+            mProgressBar.setLayoutParams(params);
+
             mRootView.addView(mProgressBar);
         }
 
