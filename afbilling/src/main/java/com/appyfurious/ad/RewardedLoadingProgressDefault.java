d07b813d67525feab1d3be04c857c5e2de38bdbb
Oleg Davidovich <o.davidovich@appyfurious.com>
2018-10-22 12:21:08 +0300

    change position of default rewarded progress 3

--- afbilling/src/main/java/com/appyfurious/ad/RewardedLoadingProgressDefault.java
+++ afbilling/src/main/java/com/appyfurious/ad/RewardedLoadingProgressDefault.java
@@ -3,10 +3,17 @@ package com.appyfurious.ad;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import com.appyfurious.afbilling.R;
 
@@ -37,12 +44,43 @@ public class RewardedLoadingProgressDefault implements RewardedLoadingProgressLi
             AppCompatActivity rewardedActivity = (AppCompatActivity) mContext;
             mRootView = rewardedActivity.findViewById(android.R.id.content);
 
-            ViewGroup.LayoutParams params = mRootView.getLayoutParams();
-            params.width = FrameLayout.LayoutParams.MATCH_PARENT;
-            params.height = 5;
-            mProgressBar.setLayoutParams(params);
-
+            ViewGroup.LayoutParams lp = mRootView.getLayoutParams();
+            lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            lp.height = 6;
             mRootView.addView(mProgressBar);
+
+            if (lp instanceof LinearLayout.LayoutParams) {
+                LinearLayout.LayoutParams llp = ((LinearLayout.LayoutParams) lp);
+                llp.gravity = Gravity.TOP;
+
+            } else if (lp instanceof FrameLayout.LayoutParams) {
+                FrameLayout.LayoutParams flp = ((FrameLayout.LayoutParams) lp);
+                flp.gravity = Gravity.TOP;
+
+            } else if (lp instanceof RelativeLayout.LayoutParams) {
+                RelativeLayout.LayoutParams rlp = ((RelativeLayout.LayoutParams) lp);
+                rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+
+            } else if (lp instanceof GridLayout.LayoutParams) {
+                GridLayout.LayoutParams glp = ((GridLayout.LayoutParams) lp);
+                glp.setGravity(Gravity.TOP);
+
+            } else if (lp instanceof ConstraintLayout.LayoutParams) {
+//                ConstraintSet constraintSet = new ConstraintSet();
+//                constraintSet.clone((ConstraintLayout) adContainer);
+//                constraintSet.connect(mAdView.getId(), ConstraintSet.BOTTOM, adContainer.getId(),
+//                        ConstraintSet.BOTTOM, 0);
+//                constraintSet.connect(mAdView.getId(), ConstraintSet.LEFT, adContainer.getId(),
+//                        ConstraintSet.LEFT, 0);
+//                constraintSet.connect(mAdView.getId(), ConstraintSet.RIGHT, adContainer.getId(),
+//                        ConstraintSet.RIGHT, 0);
+//                constraintSet.applyTo((ConstraintLayout) adContainer);
+            } else if (lp instanceof CoordinatorLayout.LayoutParams) {
+                CoordinatorLayout.LayoutParams clp = ((CoordinatorLayout.LayoutParams) lp);
+                clp.gravity = Gravity.TOP;
+            }
+
+            mProgressBar.setLayoutParams(lp);
         }
 
     }
