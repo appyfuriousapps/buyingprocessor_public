461e1ab80e64ba37f13aec29bc8d1acd8353605a
Oleg Davidovich <o.davidovich@appyfurious.com>
2018-10-22 12:27:29 +0300

    change position of default rewarded progress 4

--- afbilling/src/main/java/com/appyfurious/ad/RewardedLoadingProgressDefault.java
+++ afbilling/src/main/java/com/appyfurious/ad/RewardedLoadingProgressDefault.java
@@ -47,40 +47,41 @@ public class RewardedLoadingProgressDefault implements RewardedLoadingProgressLi
             ViewGroup.LayoutParams lp = mRootView.getLayoutParams();
             lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
             lp.height = 6;
+            mProgressBar.setLayoutParams(lp);
             mRootView.addView(mProgressBar);
 
-            if (lp instanceof LinearLayout.LayoutParams) {
-                LinearLayout.LayoutParams llp = ((LinearLayout.LayoutParams) lp);
-                llp.gravity = Gravity.TOP;
+//            if (lp instanceof LinearLayout.LayoutParams) {
+//                LinearLayout.LayoutParams llp = ((LinearLayout.LayoutParams) lp);
+//                llp.gravity = Gravity.TOP;
+//
+//            } else if (lp instanceof FrameLayout.LayoutParams) {
+//                FrameLayout.LayoutParams flp = ((FrameLayout.LayoutParams) lp);
+//                flp.gravity = Gravity.TOP;
+//
+//            } else if (lp instanceof RelativeLayout.LayoutParams) {
+//                RelativeLayout.LayoutParams rlp = ((RelativeLayout.LayoutParams) lp);
+//                rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+//
+//            } else if (lp instanceof GridLayout.LayoutParams) {
+//                GridLayout.LayoutParams glp = ((GridLayout.LayoutParams) lp);
+//                glp.setGravity(Gravity.TOP);
+//
+//            } else if (lp instanceof ConstraintLayout.LayoutParams) {
+////                ConstraintSet constraintSet = new ConstraintSet();
+////                constraintSet.clone((ConstraintLayout) adContainer);
+////                constraintSet.connect(mAdView.getId(), ConstraintSet.BOTTOM, adContainer.getId(),
+////                        ConstraintSet.BOTTOM, 0);
+////                constraintSet.connect(mAdView.getId(), ConstraintSet.LEFT, adContainer.getId(),
+////                        ConstraintSet.LEFT, 0);
+////                constraintSet.connect(mAdView.getId(), ConstraintSet.RIGHT, adContainer.getId(),
+////                        ConstraintSet.RIGHT, 0);
+////                constraintSet.applyTo((ConstraintLayout) adContainer);
+//            } else if (lp instanceof CoordinatorLayout.LayoutParams) {
+//                CoordinatorLayout.LayoutParams clp = ((CoordinatorLayout.LayoutParams) lp);
+//                clp.gravity = Gravity.TOP;
+//            }
 
-            } else if (lp instanceof FrameLayout.LayoutParams) {
-                FrameLayout.LayoutParams flp = ((FrameLayout.LayoutParams) lp);
-                flp.gravity = Gravity.TOP;
 
-            } else if (lp instanceof RelativeLayout.LayoutParams) {
-                RelativeLayout.LayoutParams rlp = ((RelativeLayout.LayoutParams) lp);
-                rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-
-            } else if (lp instanceof GridLayout.LayoutParams) {
-                GridLayout.LayoutParams glp = ((GridLayout.LayoutParams) lp);
-                glp.setGravity(Gravity.TOP);
-
-            } else if (lp instanceof ConstraintLayout.LayoutParams) {
-//                ConstraintSet constraintSet = new ConstraintSet();
-//                constraintSet.clone((ConstraintLayout) adContainer);
-//                constraintSet.connect(mAdView.getId(), ConstraintSet.BOTTOM, adContainer.getId(),
-//                        ConstraintSet.BOTTOM, 0);
-//                constraintSet.connect(mAdView.getId(), ConstraintSet.LEFT, adContainer.getId(),
-//                        ConstraintSet.LEFT, 0);
-//                constraintSet.connect(mAdView.getId(), ConstraintSet.RIGHT, adContainer.getId(),
-//                        ConstraintSet.RIGHT, 0);
-//                constraintSet.applyTo((ConstraintLayout) adContainer);
-            } else if (lp instanceof CoordinatorLayout.LayoutParams) {
-                CoordinatorLayout.LayoutParams clp = ((CoordinatorLayout.LayoutParams) lp);
-                clp.gravity = Gravity.TOP;
-            }
-
-            mProgressBar.setLayoutParams(lp);
         }
 
     }
